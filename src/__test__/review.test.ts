81bddff3e39dfb9ba52b642d528d012bd6b21673
Alexander <65241585+Alex8943@users.noreply.github.com>
2024-12-03 09:09:34 +0100

    sync before all tests

--- src/__test__/review.test.ts
+++ src/__test__/review.test.ts
@@ -1,36 +1,39 @@
 import request from 'supertest';
 import app from '../app'; // Import your Express app
-
+import sequelize from "../other_services/sequelizeConnection";
 
     jest.mock('../routes/authenticateUser', () => {
         return jest.fn((req, res, next) => next());
     });
 
-
     describe('Review Controller', () => {
-    it('should fetch excacly amount of reviews', async () => {
-        const response = await request(app).get('/reviews/25'); // Adjust the endpoint
-        expect(response.status).toBe(200);
-        expect(Array.isArray(response.body)).toBe(true);
-    });
-
-    describe('Review Controller', () => {
-        it('should create a new review with genres', async () => {
-          const reviewData = {
-            media_fk: 1, // Assuming media ID 1 exists
-            title: 'Test Review',
-            description: 'This is a test review description.',
-            platform_fk: 2, // Assuming platform ID 2 exists
-            user_fk: 3, // Assuming user ID 3 exists
-            genre_ids: [1, 2], // Assuming genre IDs 1 and 2 exist
-          };
-      
-          const response = await request(app)
-            .post('/review')
-            .send(reviewData); // Send the review data in the request body
-      
-          expect(response.status).toBe(200);
-          expect(response.body).toHaveProperty('reviewId'); // Expect response to contain the review ID
+        beforeEach(async () => {
+            await sequelize.sync({ force: true });
         });
-    });
+
+        it('should fetch excacly amount of reviews', async () => {
+            const response = await request(app).get('/reviews/25'); // Adjust the endpoint
+            expect(response.status).toBe(200);
+            expect(Array.isArray(response.body)).toBe(true);
+        });
+
+        describe('Review Controller', () => {
+            it('should create a new review with genres', async () => {
+            const reviewData = {
+                media_fk: 1, // Assuming media ID 1 exists
+                title: 'Test Review',
+                description: 'This is a test review description.',
+                platform_fk: 2, // Assuming platform ID 2 exists
+                user_fk: 3, // Assuming user ID 3 exists
+                genre_ids: [1, 2], // Assuming genre IDs 1 and 2 exist
+            };
+        
+            const response = await request(app)
+                .post('/review')
+                .send(reviewData); // Send the review data in the request body
+        
+            expect(response.status).toBe(200);
+            expect(response.body).toHaveProperty('reviewId'); // Expect response to contain the review ID
+            });
+        });
 });
