7eeba05f4a4fcd08a413853b8be085ce0243d169
Alexander <65241585+Alex8943@users.noreply.github.com>
2024-12-10 14:25:24 +0100

    removed error test

--- src/__test__/database.test.ts
+++ src/__test__/database.test.ts
@@ -4,6 +4,7 @@ import { testDBConnection } from '../db_services/db_connection';
 import { test_DB2_connection } from '../db_services/db2_connection';
 import dotenv from 'dotenv';
 import mysql from 'mysql2/promise';
+import { error } from 'console';
 
 dotenv.config();
 
@@ -21,24 +22,8 @@ describe("Test db connections", () => {
             .then(() => console.log('Connection has been established successfully.'))
             .catch((error) => console.error('Unable to connect to the database:', error));
     });
+        
 
-    it("Should fail to connect to database1 with wrong credentials", async () => {
-        const pool = mysql.createPool({
-            host: process.env.dev_host,
-            user: process.env.dev_user,
-            password: process.env.dev_password,
-            database: process.env.dev_database,
-        });
-        const connection = await pool.getConnection();
-        try {
-            await connection.ping();
-            console.log(`Connected to mysql database name: ${process.env.dev_database}`);
-        }
-        catch (err) {
-            console.log("Could not connect to mysql database");
-            process.exit(1);
-        }
-
-    });
+    
 });
 
