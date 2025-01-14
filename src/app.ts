773e6775488e08e2883d694f16d5d6c5d37e6cd5
Alex8943 <alex8943@stud.kea.dk>
2025-01-14 12:55:54 +0100

    removed seed data

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { testDBConnection } from './db_services/local/db_connection';
 import { deployed_testDBConnection } from './db_services/deployed/db_connection';
-import { seedData } from './db_services/seed_data/database1/seed_data';
+//import { seedData } from './db_services/seed_data/database1/seed_data';
 //import { seedData2 } from './db_services/seed_data/database2/seed_data';
 import createBackup from './db_services/backup';
 import cors from 'cors';
